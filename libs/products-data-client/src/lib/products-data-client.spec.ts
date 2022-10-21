3f1c96cfeefa4957621d731740f1c5758bda5656
Jordy Kokelaar <jordykokelaar@gmail.com>
2022-10-21 20:55:29 +0200

    commiting to test affected

--- libs/products-data-client/src/lib/products-data-client.spec.ts
+++ libs/products-data-client/src/lib/products-data-client.spec.ts
@@ -1,7 +1,18 @@
-import { productsDataClient } from './products-data-client';
+import {
+  createProductsDataClient,
+  exampleProducts,
+} from './products-data-client';
 
 describe('productsDataClient', () => {
-  it('should work', () => {
-    expect(productsDataClient()).toEqual('products-data-client');
+  it('should get all example products', async () => {
+    const productsDataClient = createProductsDataClient();
+    const products = await productsDataClient.getProducts();
+    expect(products).toEqual(Object.values(exampleProducts));
   });
-});
+
+  it('should get example product by id', async () => {
+    const productsDataClient = createProductsDataClient();
+    const product = await productsDataClient.getProductById('1');
+    expect(product).toEqual(exampleProducts['1']);
+  });
+})
